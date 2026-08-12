d525922f4b82b1e74d1630c8c25beb057ee276be
747389 <jtrowsdale@students.mags.school.nz>
2026-08-12 12:29:11 +1200

    trees in the level

--- i-dont-know/scenes/main_menu.gd
+++ i-dont-know/scenes/main_menu.gd
@@ -1,6 +1,8 @@
-extends Node2D
+extends CanvasLayer	
 
+const METHOD : StringName = &"change_scene_to_packed"
+const LEVEL_1 := preload("res://level/level.tscn")
 
-# Go to levle 1
+# Go to level 1
 func _on_button_pressed() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://level/level.tscn")
+	get_tree().call_deferred(METHOD, LEVEL_1)
